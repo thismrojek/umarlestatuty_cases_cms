7a2795aa67e190935a78ecb32de05fa4a1b9c3c5
Mateusz Rojek <mrojek@czooosnek.cloud>
2022-09-30 18:01:49 +0200

    Ability to use aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { dirname, resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     svelte({
       emitCss: false
-    })
-  ]
+    }),
+  ],
+  resolve:{
+    alias:{
+      '@assets' : resolve(dirname('./'), './src/assets'),
+      '@api' : resolve(dirname('./'), './src/api'),
+      '@images' : resolve(dirname('./'), './src/assets/images'),
+      '@components' : resolve(dirname('./'), './src/components'),
+      '@lib' : resolve(dirname('./'), './src/lib'),
+      '@type' : resolve(dirname('./'), './src/types'),
+      '@stores' : resolve(dirname('./'), './src/stores')
+    },
+  }
 })
