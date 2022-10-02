11eba80242ccd164117aa28051f0c3d0a0233655
Mateusz Rojek <mrojek@czooosnek.cloud>
2022-10-02 11:48:45 +0200

    draft auth

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,14 @@
 <script>
+    import CheckingCredentials from "@components/Auth/CheckingCredentials.svelte";
     import Header from "@components/Header/Header.svelte";
     import Layout from "@components/Layout.svelte";
+    import { authState } from "@stores/authState";
 </script>
 
 <Layout>
-  <Header />
+  {#if $authState}
+    <Header />
+  {:else}
+    <CheckingCredentials />
+  {/if}
 </Layout>
