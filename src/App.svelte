7b1115280faa16531f71e7410aa14f65eadd89ed
Mateusz Rojek <mrojek@czooosnek.cloud>
2022-10-09 09:40:59 +0200

    Included case list view in app

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
     import CheckingCredentials from "@components/Auth/CheckingCredentials.svelte";
+    import CasesList from "@components/Cases/CasesList.svelte";
     import Header from "@components/Header/Header.svelte";
     import Layout from "@components/Layout.svelte";
     import { authState } from "@stores/authState";
@@ -8,6 +9,7 @@
 <Layout>
   {#if $authState}
     <Header />
+    <CasesList />
   {:else}
     <CheckingCredentials />
   {/if}
